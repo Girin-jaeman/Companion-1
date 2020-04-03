f1f14ac8cc02c73e1cb1d2b75373af61592bdc3e
Jaydeen Lee <jklee050@gmail.com>
2020-04-03 18:15:04 +0900

    data table 쿼리문 작성

--- Companion/sql/admin.sql
+++ Companion/sql/admin.sql
@@ -5,4 +5,6 @@
 select * from `order_detail`;
 select * from `order`;
 select * from `product`;
-select * from `` where order_date='2020-04%';
+select product_id, product_name, product_price, product_stock, (select sum(order_detail_quantity) from `order_detail` o_d where o_d.product_id=p.product_id ) as total_cumulative_sales, (select sum(order_detail_quantity) from `order_detail` o_d, `order` o where o_d.product_id=p.product_id AND o_d.order_id=o.order_id AND o.order_date like '2020-03%') as month_cumulative_sales from `product` p;
+
+select sum(o_d.order_detail_quantity) from `order_detail` o_d, `order` o where o_d.order_id=o.order_id AND order_date like '2020-03%';
--- Companion/src/main/java/com/bit/companion/model/admin/AdminHomeDao.java
+++ Companion/src/main/java/com/bit/companion/model/admin/AdminHomeDao.java
@@ -4,24 +4,24 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.bit.companion.common.ChartDate;
-import com.bit.companion.model.entity.admin.PaymentVo;
+import com.bit.companion.model.entity.admin.AdminPaymentVo;
 import com.bit.companion.model.entity.order.ProductVo;
 
 public interface AdminHomeDao {
 
-	List<ProductVo> selectProduct() throws SQLException;
-	PaymentVo selectToday(ChartDate chartDate) throws SQLException;
-	PaymentVo selectToday_1(ChartDate chartDate) throws SQLException;
-	PaymentVo selectToday_2(ChartDate chartDate) throws SQLException;
-	PaymentVo selectToday_3(ChartDate chartDate) throws SQLException;
-	PaymentVo selectToday_4(ChartDate chartDate) throws SQLException;
-	PaymentVo selectToday_5(ChartDate chartDate) throws SQLException;
-	PaymentVo selectToday_6(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth_1(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth_2(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth_3(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth_4(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth_5(ChartDate chartDate) throws SQLException;
-	PaymentVo selectMonth_6(ChartDate chartDate) throws SQLException;
+	List<ProductVo> selectProduct(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday_1(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday_2(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday_3(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday_4(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday_5(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectToday_6(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth_1(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth_2(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth_3(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth_4(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth_5(ChartDate chartDate) throws SQLException;
+	AdminPaymentVo selectMonth_6(ChartDate chartDate) throws SQLException;
 }
